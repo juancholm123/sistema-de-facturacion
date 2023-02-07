d50236a823e076fd0ced4872e9eae473436567d8
juan camilo lopez marin <juancholm123@gmail.com>
2023-02-07 12:21:36 -0500

    empresa responsiva v4

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -6,5 +6,10 @@ controller.index_empresa = (req, res) => {
     res.render('./empresa/index')
 }
 
+controller.prefil = (req, res) => {
+    res.render('./empresa/perfil')
+}
+
+
 
 export default controller
--- src/routes/empresa.js
+++ src/routes/empresa.js
@@ -5,6 +5,7 @@ const router = Router();
 
 
 router.get('/empresa', empresa.index_empresa)
+router.get('/perfil-empresa', empresa.prefil)
 
 
 export default router;
